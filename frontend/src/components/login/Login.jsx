9406a643674ef4599f67104d9cbc9776057ee7fd
scenicbusk <kumarsumit2227@gmail.com>
2024-06-23 12:29:58 +0530

    Login and registration page added

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="h-full flex">
+      <div className="flex flex-[3] h-full items-center justify-center">
+        <form className="flex flex-col gap-5">
+          <h1 className="text-3xl">Login</h1>
+          <input className="p-5 border border-gray-600 rounded" name="email" type="text" placeholder="Email" />
+          <input className="p-5 border border-gray-600 rounded" name="password" type="password" placeholder="Password" />
+          <button className="p-5 rounded bg-teal-700 text-white font-bold cursor-pointer disabled:bg-[#bed9d8] disabled:cursor-not-allowed">Login</button>
+          <Link className="text-blue-500 hover:text-blue-700" to="/register">Don't you have an account?</Link>
+        </form>
+      </div>
+      <div className="hidden sm:flex flex-[2] bg-[#fcf5f3] items-center justify-center">
+        <img className="w-full" src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <div className="h-full flex">
+      <div className="flex flex-[3] h-full items-center justify-center">
+        <form className="flex flex-col gap-5">
+          <h1 className="text-3xl">Create an Account</h1>
+          <input className="p-5 border border-gray-600 rounded" name="username" type="text" placeholder="Username" />
+          <input className="p-5 border border-gray-600 rounded" name="email" type="text" placeholder="Email" />
+          <input className="p-5 border border-gray-600 rounded" name="password" type="password" placeholder="Password" />
+          <button className="p-5 rounded bg-teal-700 text-white font-bold cursor-pointer disabled:bg-[#bed9d8] disabled:cursor-not-allowed">Register</button>
+          <Link className="text-blue-500 hover:text-blue-700" to="/login">Do you have an account?</Link>
+        </form>
+      </div>
+      <div className="hidden sm:flex flex-[2] bg-[#fcf5f3] items-center justify-center">
+        <img className="w-full" src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
